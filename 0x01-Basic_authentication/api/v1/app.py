7b9f2c10c9c551c52f31305cdd83c2ab9e2e7fe6
favvie <favourabangwu@gmail.com>
2023-05-04 17:46:34 +0100

    update auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
@@ -17,6 +16,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 if os.environ['AUTH_TYPE'] == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
