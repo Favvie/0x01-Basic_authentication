2ab4144624f06ef5e56040b75563c961ed02ca73
favvie <favourabangwu@gmail.com>
2023-05-04 05:57:06 +0100

    update the shebang line

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 """the authentication module"""
 
 from flask import request
@@ -9,7 +9,15 @@ class Auth:
     """The base authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """check if the path requires authentication"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if path.endswith('/'):
+            pass
+        else:
+            path = path + "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """checks if the request has an authorization header"""
