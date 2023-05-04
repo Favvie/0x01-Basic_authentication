9d3d735d476fb86efed8f258bf1d577d9b054ac2
favvie <favourabangwu@gmail.com>
2023-05-04 18:58:02 +0100

    task 9: extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -32,3 +33,16 @@ class BasicAuth(Auth):
             return None
         value = value.decode('utf-8')
         return value
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> Tuple[str, str]:
+        """extract user credentials from decoded base64 str"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        username = decoded_base64_authorization_header.split(":")[0]
+        userpwd = decoded_base64_authorization_header.split(":")[1]
+        return (username, userpwd)
