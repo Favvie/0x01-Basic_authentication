7e9e1b09836fa5269dae6fe61d0f90b6555f7bc1
favvie <favourabangwu@gmail.com>
2023-05-04 18:45:54 +0100

    task 8: decode base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """decode a base64 encoding into utf-8"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            value = base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        value = value.decode('utf-8')
+        return value
