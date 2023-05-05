60eb1a7002358a10db03384400656f5752056cbb
favvie <favourabangwu@gmail.com>
2023-05-05 11:41:32 +0100

    task 0: session authentication

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,6 +27,10 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+    if user_id == 'me' and request.curent_user is not None:
+        return jsonify(request.current_user)
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -120,3 +124,9 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=True)
+def get_auth_user():
+    """retrieve auth user object"""
+    return jsonify(request.current_user.to_json())
