1910de7dee2aa1f201674bf534d85e216fd141a5
favvie <favourabangwu@gmail.com>
2023-05-10 14:45:03 +0100

    add init method for user class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,8 +10,16 @@ class User(Base):
     """User Model"""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
+
+    def __init__(self, email, hashed_password):
+        """initialization"""
+        self.email = email
+        self.hashed_password = hashed_password
+
+    def __repr__(self):
+        return f"{self.id} {self.email} {self.hashed_password}"
