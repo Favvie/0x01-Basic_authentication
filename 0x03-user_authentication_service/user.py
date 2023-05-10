be8267856acabbb12b23994456f177502d3d71b1
favvie <favourabangwu@gmail.com>
2023-05-10 15:08:33 +0100

    remove init from user class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,11 +15,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
-
-    def __init__(self, email, hashed_password):
-        """initialization"""
-        self.email = email
-        self.hashed_password = hashed_password
-
-    def __repr__(self):
-        return f"{self.id} {self.email} {self.hashed_password}"
