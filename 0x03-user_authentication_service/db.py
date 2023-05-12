a0019aaffbee129fe8a00708a7f3faa7057c2e6e
favvie <favourabangwu@gmail.com>
2023-05-12 12:36:33 +0100

    add variable annotations to find user by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
             self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """find user by email"""
         if not kwargs:
             raise InvalidRequestError("Invalid query arguments.")
