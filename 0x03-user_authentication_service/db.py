38af1df4c1bdb5c75a7578b2c46b3bcc00a95b64
favvie <favourabangwu@gmail.com>
2023-05-12 11:11:25 +0100

    find user by keyword args

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,4 +39,14 @@ class DB:
         if self._session is not None:
             self._session.add(user)
             self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """find user by email"""
+        if not kwargs:
+            raise InvalidRequestError("Invalid query arguments.")
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound("No user found")
         return user
